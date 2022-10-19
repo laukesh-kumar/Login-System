d46fa3335dd8f5231f97f09199b81f693dc72143
laukesh kumar <76173001+laukesh-kumar@users.noreply.github.com>
2022-10-19 20:29:54 +0530

    Add files via upload
    Initial Commit

---
+++ contact.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Contact Us!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Php Login System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+
+      
+     
+    </ul>
+  </div>
+</nav>
+
+    <!-- Contct form  -->
+    <div class="container w-50 m-auto">
+        <div class="text">
+            <h1 class="text-center">Contact Us</h1>
+            <hr class="w-25 m-auto bg-dark">
+        </div>
+        <form action="action.php" method="POST" autocomplete="off">
+            <div class="user my-4">
+              <label for="username">Username: </label>
+              <input type="text" name="username" id="username" class="form-control" required>
+            </div>
+            <div class="email my-4">
+              <label for="email">E-mail: </label>
+              <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="contact my-4">
+              <label for="contact">Contact: </label>
+              <input type="number" name="contact" id="contact" class="form-control" required>
+            </div>
+            <div class="address my-4">
+              <label for="address">Address: </label>
+              <input type="text" name="address" id="address" class="form-control" required>
+            </div>
+            <div class="message my-4">
+              <label for="message">Message: </label>
+              <textarea name="message" id="message" class="form-control" cols="30" rows="5"></textarea>
+            </div>
+            <button class="btn btn-primary">Send Message</button>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
